ce99ef47592951e3e53622b193b937ee80432469
Aryan Goel <aryangoel600@gmail.com>
2026-07-03 09:49:00 +0530

    Add coin pickup functionality in coin.gd

---
+++ coin.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var game_manager: Node = %GameManager
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _on_body_entered(body):
+	game_manager.add_point()
+	print("+1 Coin..")
+	animation_player.play("Pickup")
